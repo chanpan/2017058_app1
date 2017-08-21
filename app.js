c04e8e5bf3d25938fbf66e0c0ddaa662c2e6ba96
aomruk <aomruk12123@gmail.com>
2017-08-21 14:21:12 +0700

    mastet

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
- 
+import mysql from 'mysql';
+
 var HelloBox = React.createClass({
- 
-  render: function() {
-    return (
-      <div className="helloTag">
-      Hello world from ReactJS
-      </div>
-    );
-  }
+	function xxx(){
+		 
+		var connection = mysql.createConnection({
+		  host     : 'localhost',
+		  user     : 'damasac',
+		  password : 'damasac!@#$%',
+		  database : 'tdc_online'
+		});
+		 
+		connection.connect();
+		 
+		connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+		  if (error) throw error;
+		  console.log('The solution is: ', results[0].solution);
+		});
+		 
+		connection.end();
+	}
+    render: function() {
+        return ( 
+            <div className = "helloTag" >
+            Hello world from ReactJS 
+            </div>
+        );
+    }
 });
- 
-ReactDOM.render(<HelloBox/>, document.getElementById('helloTag'));
+
+ReactDOM.render( < HelloBox / > , document.getElementById('helloTag'));
